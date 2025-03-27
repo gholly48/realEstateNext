e527955bbbf1aa40f2c53ca1f34a90f0a92f95f4
gholly48 <gholly48@outlook.com>
2025-03-27 23:52:38 +0330

    Register Page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link"
+
 const Register = () => {
 
     return (
-        <h1>Register</h1>
+    <div className="p-3 max-w-lg mx-auto">
+        <h1 className="text-3xl text-center font-semibold my-7">Register</h1>
+        <form className="flex flex-col gap-4">
+            <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" />
+            <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email" />
+            <input type="password" placeholder="password" className="border p-3 rounded-lg" id="password" />
+            <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Register</button>
+        </form>
+        <div className="flex justify-center gap-2 mt-5">
+            <p>Have an account?</p>
+            <Link href={'/login'}>
+              <span className="text-blue-700">Login</span>
+            </Link>
+        </div>
+    </div>
     )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,20 @@
-'use client'; // چون این فانکشن روی کلیک اجرا می‌شه، باید client باشد.
+async function getUsers() {
+  const res = await fetch('http://localhost:1234/users', { cache: 'no-store' }); 
+  if (!res.ok) throw new Error('Failed to fetch users');  
+  return res.json();
+}
 
-import { useState } from 'react';
-
-export default function Home() {
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [password, setPassword] = useState('');
-
-  async function createUser() {
-    const res = await fetch('http://localhost:1234/users', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, password }),
-    });
-
-    if (res.ok) {
-      setMessage('✅ کاربر با موفقیت ایجاد شد!');
-      setName('');
-    } else {
-      setMessage('❌ خطایی رخ داد!');
-    }
-  } 
+export default async function Home() {
+  const users = await getUsers();
 
   return (
     <div>
-      <h1>افزودن کاربر جدید</h1>
-           <input value={name} onChange={(e) => setName(e.target.value)} placeholder="نام را وارد کنید" />
-           <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="رمز عبور را وارد کنید" />
-         <button onClick={createUser}>افزودن</button>
-      <p>{message}</p>
+      <h1>لیست کاربران</h1>
+      <ul>
+        {users.map((user: { id: number; name: string }) => (
+          <li >{user.id} {user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
